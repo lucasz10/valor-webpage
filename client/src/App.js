3f80508ce00ceb2c344b3a2a4334d834ff12b724
lucasz10 <lucas.zach10@gmail.com>
2023-09-07 17:52:41 -0700

    Cleaned up files. Did not need server side folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Import react-bootstrap styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Imported components
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-// Imported pages for Routing
-import LoginPage from './pages/LoginPage';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <div className="flex-column text-center align-center min-100-vh">
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  );
+  return <Router></Router>;
 }
 
 export default App;
--- server/models/index.js
+++
@@ -1,5 +0,0 @@
-const Topping = require('./Topping');
-const Pizza = require('./Pizza');
-const User = require('./User');
-
-module.exports = { Topping, Pizza, User };
